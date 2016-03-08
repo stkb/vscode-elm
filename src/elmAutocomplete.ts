774e450c70e26cf5411b9f0d7f206a7d66b087c6
Steve Baker <_steve_@outlook.com>
2016-03-08 09:05:49 +0100

    save

--- src/elmAutocomplete.ts
+++ src/elmAutocomplete.ts
@@ -1,8 +1,26 @@
+import { join } from 'path';
 import * as vscode from 'vscode';
+import { Position, Range } from 'vscode'
+
 import * as oracle from './elmOracle'
+import { detectProjectRoot } from './elmUtils'
 
 export class ElmCompletionProvider implements vscode.CompletionItemProvider {
-  public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]> {
+
+  public provideCompletionItems(
+    document: vscode.TextDocument,
+    position: Position,
+    token: vscode.CancellationToken)
+    : Thenable<vscode.CompletionItem[]>
+  {
+    const lineTextUpToCursor =
+      document.lineAt(position).text.substr(0, position.character)
+    let match: RegExpMatchArray
+
+    if (match = lineTextUpToCursor.match(/import\s+(\S*)/)) {
+      console.log('import-' + match[1]);
+    }    
+    
     return oracle.GetOracleResults(document, position)
       .then((result) => {
         var r = result.map((v, i, arr) => {
@@ -16,4 +34,30 @@ export class ElmCompletionProvider implements vscode.CompletionItemProvider {
         return r;
       });
     }
+}
+
+function getAvailableModulesToImport(fileName: string)
+{
+  const packageDocFiles = getPackageDocFileNames(fileName);
+  packageDocFiles
+    .map(require)
+    .map(console.log)
+}
+
+function getPackageDocFileNames(fileName: string): string[]
+{
+  const rootDir = detectProjectRoot(fileName);
+
+  // If we're not in a project, no packages available, so return empty array
+  if (!rootDir) {
+    return []
+  }
+
+  else {
+    const stuffDir = join(rootDir, 'elm-stuff')
+    const depsObj = require(join(stuffDir, 'exact-dependencies.json'));
+
+    return Object.keys(depsObj)
+      .map(dep => join(stuffDir, 'packages', dep, depsObj[dep], 'documentation.json'))
+  }  
 }
